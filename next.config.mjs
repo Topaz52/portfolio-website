2eea8761b40b4edb76f445cc986ca17a93c36c65
Topaz52 <isml.msrn11@gmail.com>
2024-09-11 13:35:48 +0800

    Update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+        NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+      // Add other environment variables here
+    },
+    // Other Next.js config options can be added here
+  };
+  
+  export default nextConfig;
+  
