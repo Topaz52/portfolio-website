f0f2119e14e94de5faadece65164e81ad7830d12
Topaz52 <isml.msrn11@gmail.com>
2024-09-12 15:43:31 +0800

    Some changes on Home, Project section

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -4,16 +4,17 @@ interface ProjectCardProps {
     title: string;
     description: string;
     urlLink: string;
+    githubLink: string;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, urlLink }) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, urlLink, githubLink }) => {
     return (
         <div className="bg-primary-800 rounded-xl p-6 shadow-lg" >
             <div className="flex justify-between items-center mb-4">
                 <a href={urlLink} target="_blank" rel="noopener noreferrer">
                     <h3 className="text-white text-xl font-bold">{title}</h3>
                 </a>
-                <a href={urlLink} target="_blank" rel="noopener noreferrer" className="bg-secondary-800 hover:bg-secondary-500 text-white py-1 px-3 rounded-full text-sm flex items-center space-x-2">
+                <a href={githubLink} target="_blank" rel="noopener noreferrer" className="bg-secondary-800 hover:bg-secondary-500 text-white py-1 px-3 rounded-full text-sm flex items-center space-x-2">
                     <span>GITHUB</span>
                     <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                         <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.22 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.13 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.52-1.04 2.2-.82 2.2-.82.44 1.11.16 1.93.08 2.13.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.64 3.95.29.25.54.73.54 1.47 0 1.06-.01 1.92-.01 2.18 0 .21.15.46.55.38C13.71 14.53 16 11.54 16 8c0-4.42-3.58-8-8-8z" />
--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -9,25 +9,36 @@ const projectsData = [
         id: 1,
         title: "Phishing Website Detection",
         description: "This project used machine learning to determine if a website is safe. It found that deep learning models, especially Bi-LSTM, were most accurate at detecting phishing websites.",
-        urlLink: "https://github.com/Topaz52/Phishing-Website-Detection-using-RNN-FYP-"
+        urlLink: "https://github.com/Topaz52/Phishing-Website-Detection-using-RNN-FYP-",
+        githubLink: "https://github.com/Topaz52/Phishing-Website-Detection-using-RNN-FYP-"
     },
     {
         id: 2,
         title: "Asset Tracking IoT",
         description: "This project created a device that tracks things using GPS and sends their location via text messages. It uses sensors and a microcontroller to determine location and orientation. The device is useful for preventing theft.",
-        urlLink: "https://github.com/Topaz52/Asset-Tracking-IOT"
+        urlLink: "https://github.com/Topaz52/Asset-Tracking-IOT",
+        githubLink: "https://github.com/Topaz52/Asset-Tracking-IOT"
     },
     {
         id: 3,
         title: "Heart Disease using Fuzzy",
         description: "Fuzzy logic can help doctors diagnose heart disease more accurately. It considers multiple factors, like heartbeats and medical history, to provide a better understanding of a patient's risk. This is especially helpful for people who might not have obvious symptoms.",
-        urlLink: "https://github.com/Topaz52/Heart-Disease-using-fuzzy"
+        urlLink: "https://github.com/Topaz52/Heart-Disease-using-fuzzy",
+        githubLink: "https://github.com/Topaz52/Heart-Disease-using-fuzzy"
     },
     {
         id: 4,
         title: "Aircraft Arrival Sequencing and Scheduling (AASS) with Genetic Algorithm",
         description: "AASS with Genetic Algorithm is a system that uses a computer program to figure out the best order and timing for planes to land at an airport. This helps to avoid delays and keep things safe.",
-        urlLink: "https://github.com/Topaz52/Aircraft-Arrival-Sequencing-and-Scheduling-with-Genetic-Algorithm"
+        urlLink: "https://github.com/Topaz52/Aircraft-Arrival-Sequencing-and-Scheduling-with-Genetic-Algorithm",
+        githubLink: "https://github.com/Topaz52/Aircraft-Arrival-Sequencing-and-Scheduling-with-Genetic-Algorithm"
+    },
+    {
+        id: 5,
+        title: "Save to Amazon S3 Storage Telegram Bot",
+        description: "This bot allows you to upload files sent via Telegram directly to Amazon S3 storage. Simply send a file to the bot, and it will handle the upload to your S3 bucket, providing a secure and efficient way to manage your files in the cloud. Containerize the application using Docker and deploy it on Google Cloud Platform with Google Artifact Registry and Cloud Run.",
+        urlLink: "https://t.me/Save2S3Bot",
+        githubLink: "https://github.com/Topaz52"
     },
 ]
 
@@ -77,6 +88,7 @@ const ProjectsSection = () => {
                                 title={project.title}
                                 description={project.description}
                                 urlLink={project.urlLink}
+                                githubLink={project.githubLink}
                             />
                         )}
                     </div>
